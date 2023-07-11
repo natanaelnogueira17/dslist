37a7340f31d78ce281dddb4e4fb96d3eeeb2396d
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-11 09:22:30 -0300

    project created

---
+++ src/main/java/com/treinamento/TreinaApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinaApplication.class, args);
+	}
+
+}
