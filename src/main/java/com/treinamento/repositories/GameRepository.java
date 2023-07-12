bec79aaeba86b7c5041d169a16e4c7cb07eef86f
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-12 09:06:36 -0300

    GameService, GameController, GameRepository

---
+++ src/main/java/com/treinamento/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.treinamento.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.treinamento.entitys.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+	
+}
